511b7b57bf678c80b8baefc9aba71f82c2ef1011
Shawn Ma <qsma@thoughtworks.com>
2015-10-29 17:35:49 +0800

    refactory

--- update.sh
+++ update.sh
@@ -13,13 +13,13 @@ versions=( "${versions[@]%/}" )
 for version in "${versions[@]}"; do	
   if [ $version == "2.7" ]; then
   	package="python"
-    fullVersion="$(curl -fsSL "http://archive.ubuntu.com/ubuntu/dists/trusty/main/binary-amd64/Packages.gz" | gunzip | awk -F ': ' '$1 == "Package" { pkg = $2 } pkg == "python" && $1 == "Version" { print $2 }' | sort -rV | head -n1 )"
+    fullVersion="$(curl -fsSL "http://archive.ubuntu.com/ubuntu/dists/trusty/main/binary-amd64/Packages.gz"                  | gunzip | awk -v pkgname=$package -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | sort -rV | head -n1 )"
   elif [ $version == "3.4" ]; then
   	package="python3.4"
-  	fullVersion="$(curl -fsSL "http://archive.ubuntu.com/ubuntu/dists/trusty/main/binary-amd64/Packages.gz" | gunzip | awk -F ': ' '$1 == "Package" { pkg = $2 } pkg == "python3.4" && $1 == "Version" { print $2 }' | sort -rV | head -n1 )"
+  	fullVersion="$(curl -fsSL "http://archive.ubuntu.com/ubuntu/dists/trusty/main/binary-amd64/Packages.gz"                  | gunzip | awk -v pkgname=$package -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | sort -rV | head -n1 )"
   else
   	package="python${version}"
-    fullVersion="$(curl -fsSL "http://ppa.launchpad.net/fkrull/deadsnakes/ubuntu/dists/trusty/main/binary-amd64/Packages.gz" | gunzip | awk -F ': ' '$1 == "Package" { pkg = $2 } pkg == "python"'"$version"'"" && $1 == "Version" { print $2 }' | sort -rV | head -n1 )"
+    fullVersion="$(curl -fsSL "http://ppa.launchpad.net/fkrull/deadsnakes/ubuntu/dists/trusty/main/binary-amd64/Packages.gz" | gunzip | awk -v pkgname=$package -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | sort -rV | head -n1 )"
 	fi
 	(
 		set -x
