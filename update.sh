b327fe3b261fe2199018951f302e6c53cd4dda9f
Shawn Ma <qsma@thoughtworks.com>
2015-11-06 09:18:01 +0800

    update

--- update.sh
+++ update.sh
@@ -12,21 +12,21 @@ versions=( "${versions[@]%/}" )
 
 for version in "${versions[@]}"; do	
   if [ $version == "2.7" ]; then
-  	package="python"
-    fullVersion="$(curl -fsSL "http://archive.ubuntu.com/ubuntu/dists/trusty/main/binary-amd64/Packages.gz"                  | gunzip | awk -v pkgname=$package -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | sort -rV | head -n1 )"
+  	fullPackage="python"
+    fullVersion="$(curl -fsSL "http://archive.ubuntu.com/ubuntu/dists/trusty/main/binary-amd64/Packages.gz"                  | gunzip | awk -v pkgname=$fullPackage -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | sort -rV | head -n1 )"
   elif [ $version == "3.4" ]; then
-  	package="python3.4"
-  	fullVersion="$(curl -fsSL "http://archive.ubuntu.com/ubuntu/dists/trusty/main/binary-amd64/Packages.gz"                  | gunzip | awk -v pkgname=$package -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | sort -rV | head -n1 )"
+  	fullPackage="python3.4"
+  	fullVersion="$(curl -fsSL "http://archive.ubuntu.com/ubuntu/dists/trusty/main/binary-amd64/Packages.gz"                  | gunzip | awk -v pkgname=$fullPackage -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | sort -rV | head -n1 )"
   else
-  	package="python${version}"
-    fullVersion="$(curl -fsSL "http://ppa.launchpad.net/fkrull/deadsnakes/ubuntu/dists/trusty/main/binary-amd64/Packages.gz" | gunzip | awk -v pkgname=$package -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | sort -rV | head -n1 )"
+  	fullPackage="python${version}"
+    fullVersion="$(curl -fsSL "http://ppa.launchpad.net/fkrull/deadsnakes/ubuntu/dists/trusty/main/binary-amd64/Packages.gz" | gunzip | awk -v pkgname=$fullPackage -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | sort -rV | head -n1 )"
 	fi
 	(
 		set -x
 		sed '
-			s/%%MAJOR%%/'"$version"'/g;
-			s/%%VERSION%%/'"$fullVersion"'/g;
-			s!%%PACKAGE%%!'"$package"'!g;
+			s/%%PYTHON_MAJOR%%/'"$version"'/g;
+			s/%%PYTHON_VERSION%%/'"$fullVersion"'/g;
+			s!%%PYTHON_PACKAGE%%!'"$fullPackage"'!g;
 		' Dockerfile.template > "$version/Dockerfile"
 	)
 done
